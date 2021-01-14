81e56e646888b89785383e5a011fa554487224b3
Carlos Ortiz <c>
2021-01-14 15:51:13 -0500

    avance

---
+++ AED.py
@@ -0,0 +1,43 @@
+#importar módulos
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#importar base de datos
+
+#data.head(), data.tail()
+
+#data.shape
+
+#data.info()
+#data.dftypes
+
+#data.describe
+
+#data.columns
+
+#data.nunique()
+
+#data['variable'].unique() ver los valores de las variables
+
+#data.isnull().sum()
+#data.dropna() drop null values
+
+#drop unnecessary columns
+#rename columns if necessary
+#data.duplicated() check duplicated values
+#data.count() check number of values
+#df.drop_duplicates()
+
+#relationship analysis
+
+#data.corr()
+#sns.heatmap(data.corr())
+#sns.pairplot(data)
+#sns.relplot()
+#sns.displot()
+#sns.catplot()
+
+
+
